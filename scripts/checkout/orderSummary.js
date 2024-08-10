47efb4bb170c565a8c81628386b098cabe6d5e28
Oham Ugochukwu <ohamnelson@gmail.com>
2024-08-10 01:03:52 -0600

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -2,13 +2,9 @@ import { cart, removeFromCart, updateDeliveryOption} from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 
-
-
-const today = dayjs();
-// const deliveryDate =today.add(14, 'days');
-// console.log(deliveryDate.format('dddd, MMMM D'))
 
 export function renderOrderSummary () {
 
@@ -120,6 +116,8 @@ export function renderOrderSummary () {
                 const container = document.querySelector(`.js-cart-item-container-${productId}`);
                 // used to remove an element for the page
                 container.remove();
+
+                renderPaymentSummary();
             });
         });
 
@@ -128,7 +126,8 @@ export function renderOrderSummary () {
             radioSelector.addEventListener('click', () => {
                 const {productId, deliveryOptionId} = radioSelector.dataset;
                 updateDeliveryOption(productId, deliveryOptionId)
-                renderOrderSummary()
+                renderOrderSummary();
+                renderPaymentSummary();
 
             })
         })
