2f7dcc6c3116ce465a9911d2badb37910da21035
Oham Ugochukwu <ohamnelson@gmail.com>
2024-08-14 23:12:06 -0600

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,85 +1,90 @@
 import {cart, addToCart} from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-let productsHTML = '';
 
-products.forEach((product) => {
-    productsHTML += `
-            <div class="product-container">
-                <div class="product-image-container">
-                    <img class="product-image"
-                    src="${product.image}">
-                </div>
+loadProducts(renderProductsGrid);
 
-                <div class="product-name limit-text-to-2-lines">
-                    ${product.name}
-                </div>
+function renderProductsGrid() {
+    let productsHTML = '';
 
-                <div class="product-rating-container">
-                    <img class="product-rating-stars"
-                    src=${product.getStarsUrl()}>
-                    <div class="product-rating-count link-primary">
-                    ${product.rating.count}
+    products.forEach((product) => {
+        productsHTML += `
+                <div class="product-container">
+                    <div class="product-image-container">
+                        <img class="product-image"
+                        src="${product.image}">
                     </div>
-                </div>
 
-                <div class="product-price">
-                    ${product.getPrice()}
-                </div>
+                    <div class="product-name limit-text-to-2-lines">
+                        ${product.name}
+                    </div>
 
-                <div class="product-quantity-container">
-                    <select>
-                    <option selected value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    </select>
-                </div>
-                
-                ${product.extraInfoHTML()}
+                    <div class="product-rating-container">
+                        <img class="product-rating-stars"
+                        src=${product.getStarsUrl()}>
+                        <div class="product-rating-count link-primary">
+                        ${product.rating.count}
+                        </div>
+                    </div>
 
-                <div class="product-spacer"></div>
+                    <div class="product-price">
+                        ${product.getPrice()}
+                    </div>
 
-                <div class="added-to-cart">
-                    <img src="images/icons/checkmark.png">
-                    Added
-                </div>
+                    <div class="product-quantity-container">
+                        <select>
+                        <option selected value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        </select>
+                    </div>
+                    
+                    ${product.extraInfoHTML()}
 
-                <button class="add-to-cart-button button-primary js-add-to-cart"
-                data-product-id="${product.id}">
-                    Add to Cart
-                </button>
-        </div>`;
-})
+                    <div class="product-spacer"></div>
 
-// polymorphism: ability to call a method with knowing its class or using if statements. Line46
+                    <div class="added-to-cart">
+                        <img src="images/icons/checkmark.png">
+                        Added
+                    </div>
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
-function updateCartQuantity() {
-    let cartQuantity = 0
-            cart.forEach((cartItem) => {
-                cartQuantity += cartItem.quantity
-            })
-            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
-}
-document.querySelectorAll('.js-add-to-cart')
-    .forEach((button) => {
-        button.addEventListener('click', () => {
-        //  use data attrribute to attach the name to the button
-        // and use dataset to retrieve the name
-            const productId = button.dataset.productId;
-
-            addToCart(productId);
-            updateCartQuantity();
-        })
-       
+                    <button class="add-to-cart-button button-primary js-add-to-cart"
+                    data-product-id="${product.id}">
+                        Add to Cart
+                    </button>
+            </div>`;
     })
+
+    // polymorphism: ability to call a method with knowing its class or using if statements. Line46
+
+    document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+    function updateCartQuantity() {
+        let cartQuantity = 0
+                cart.forEach((cartItem) => {
+                    cartQuantity += cartItem.quantity
+                })
+                document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    }
+    document.querySelectorAll('.js-add-to-cart')
+        .forEach((button) => {
+            button.addEventListener('click', () => {
+            //  use data attrribute to attach the name to the button
+            // and use dataset to retrieve the name
+                const productId = button.dataset.productId;
+
+                addToCart(productId);
+                updateCartQuantity();
+            })
+        
+        })
+    }
